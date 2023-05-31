e7934575fab9b1877062525aa77bd026dd828474
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-05-31 14:30:49 +0200

    Add tables to db context

--- WorkingWithEFCore/Data/Northwind.cs
+++ WorkingWithEFCore/Data/Northwind.cs
@@ -4,18 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkingWithEFCore.Models;
 
 namespace WorkingWithEFCore.Data
 {
     public class Northwind : DbContext
     {
+        public DbSet<Category>? Categories{ get; set; } // map to table name!
+        public DbSet<Product>? Products { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
-
             string connection = $"Filename={path}";
+            optionsBuilder.UseSqlite(connection);
+        }
 
-            optionsBuilder.UseSqlite(connection);            
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // use fluent api to define stuff instead of attributes in model
+
+            modelBuilder.Entity<Category>()
+                .Property(category => category.CategoryName)
+                .IsRequired()
+                .HasMaxLength(15);
+            
+            if (Database.ProviderName?.Contains("SQlite") ?? false)
+            {
+                modelBuilder.Entity<Product>()
+                    .Property(product => product.Cost)
+                    .HasConversion<double>();
+            }
         }
     }
 }
