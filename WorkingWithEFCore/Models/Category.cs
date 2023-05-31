34104cf95a44793a3edd7924164976b0359ef323
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-05-31 14:22:59 +0200

    Add models

---
+++ WorkingWithEFCore/Models/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithEFCore.Models
+{
+    public class Category
+    {
+        // map to database pk / identity
+        public int CategoryId { get; set; }
+
+        // map to database
+        public string? CategoryName { get; set; }
+
+        // map to database different type
+        [Column(TypeName ="ntext")]
+        public string? Description { get; set; }
+
+        // navigation property
+        public virtual ICollection<Product> Products { get; set; }
+
+        // constructor
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+    }
+}
---
+++ WorkingWithEFCore/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithEFCore.Models
+{
+    public class Product
+    {
+        //  map pk to database
+        public int ProdyuctId { get; set; }
+
+        // map with extra attributes
+        [Required]
+        [StringLength(40)]
+        public string ProductName { get; set; } = null!; // null forgiving operator: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-forgiving
+
+        // map with different name
+        [Column("UnitPrice", TypeName = "money")]
+        public decimal? Cost { get; set; }
+
+        [Column("UnitsInStock")]
+        public short? Stock { get; set; }
+
+        // map to database column
+        public bool Discontinued { get; set; }
+
+        // fk
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; } = null!;
+    }
+}
