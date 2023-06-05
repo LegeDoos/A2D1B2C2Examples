926113ee054a152ce60b1e080e880329194f8a85
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-06-05 14:48:43 +0200

    Database context, fluent API, data seed en migrations

--- CodeFirstExample/CodeFirstExample/Data/VoorbeeldDb.cs
+++ CodeFirstExample/CodeFirstExample/Data/VoorbeeldDb.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CodeFirstExample.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,29 @@ namespace CodeFirstExample.Data
 {
     public class VoorbeeldDb : DbContext
     {
+        public DbSet<Voorbeeld> Voorbeelden { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = @"Data Source=.;Initial Catalog=VoorbeeldA2D1B2C2;Integrated Security=true;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // specificeer 
+            modelBuilder.Entity<Voorbeeld>()
+                .Property(v => v.Name)
+                .HasMaxLength(30);
+
+            // data seed
+            Voorbeeld vb = new Voorbeeld()
+            {
+                Id = 1,
+                Name = "Test"
+            };
+            modelBuilder.Entity<Voorbeeld>()
+                .HasData(vb);
+        }
     }
 }
---
+++ CodeFirstExample/CodeFirstExample/Migrations/20230605123350_AddVoorbeeldToDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirstExample.Migrations
+{
+    public partial class AddVoorbeeldToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Voorbeelden",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voorbeelden", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Voorbeelden",
+                columns: new[] { "Id", "CreatedOn", "Name" },
+                values: new object[] { 1, new DateTime(2023, 6, 5, 14, 33, 50, 552, DateTimeKind.Local).AddTicks(3148), "Test" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Voorbeelden");
+        }
+    }
+}
