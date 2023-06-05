87b4dd2a5e64c55dad68283200a954841b8b7a6a
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-06-05 14:06:31 +0200

    Rename project and solution (typo)

--- CodeFirstExample/CodeFirstExample/Data/VoorbeeldDb.cs
+++ CodeFirstExample/CodeFirstExample/Data/VoorbeeldDb.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeFirstExsample.Data
+namespace CodeFirstExample.Data
 {
     public class VoorbeeldDb : DbContext
     {
