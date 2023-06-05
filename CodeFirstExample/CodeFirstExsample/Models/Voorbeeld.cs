373e23de63c1d3d2927b8b2cc8775504dd57d332
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-06-05 13:53:00 +0200

    Add model

---
+++ CodeFirstExample/CodeFirstExsample/Models/Voorbeeld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExsample.Models
+{
+    public class Voorbeeld
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
