63fc85b091df08dfab4bdc9fdf0f0f4cef04d890
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-06-06 16:56:14 +0200

    Add ContextExtensions

---
+++ Chapter12/Northwind.Common.DataContext.SqlServer/NorthwindContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Shared
+{
+    public static class NorthwindContextExtensions
+    {
+        // zie boek blz 551
+        public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string connecttionstring = "Data Source=.;Initial Catalog=NorthwindData;Integrated Security=True;TrustServerCertificate=True;") 
+        {
+            services.AddDbContext<NorthwindDataContext>(options =>
+            {
+                options.UseSqlServer(connecttionstring);
+                //  options.LogTo(WriteLine, new[] { Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting });
+            });
+            return services;
+
+        }
+
+    }
+}
