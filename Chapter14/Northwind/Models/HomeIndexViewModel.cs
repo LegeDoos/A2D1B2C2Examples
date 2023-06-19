f0ca823a7284ac2dd93e494341bc17c48dc5fb2f
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-06-19 13:15:29 +0200

    Create viewmodel

---
+++ Chapter14/Northwind/Models/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Northwind.Shared;
+
+namespace Northwind.Models;
+
+public record HomeIndexViewModel
+(
+    int VisitorCount,
+    IList<Category> Categories,
+    IList<Product> Products
+);
+
+
